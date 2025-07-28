2c0315ace923d52fe929968fc3c1848cd369e426
Eric Petersen <ericpetersen3.14@gmail.com>
2025-07-28 13:10:38 -0600

    change name

--- build.zig
+++ build.zig
@@ -1,7 +1,7 @@
 const std = @import("std");
 
 pub fn build(b: *std.Build) void {
-    _ = b.addModule("bitstream", .{
+    _ = b.addModule("BitStream", .{
         .root_source_file = b.path("main.zig"),
         .target = b.standardTargetOptions(.{}),
         .optimize = b.standardOptimizeOption(.{}),
